f2ceba830198d09ddb02db51be7e3b8d71c858cf
Fyru <fyruvlad@gmail.com>
2020-04-22 15:23:09 +0300

    Create new react app using hooks

---
+++ react_hooks_app/build/precache-manifest.277e2df15315db6c40ce45a42f89de7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064ff5336a2018dd04277b0823e19dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab8f87d5c7188aba2aa",
+    "url": "/static/css/main.8b685100.chunk.css"
+  },
+  {
+    "revision": "3818da5f66ed85337be0",
+    "url": "/static/js/2.df5e99ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df5e99ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab8f87d5c7188aba2aa",
+    "url": "/static/js/main.8dffad59.chunk.js"
+  },
+  {
+    "revision": "506f9d038bd3579fa4a3",
+    "url": "/static/js/runtime-main.7c9b6090.js"
+  }
+]);
---
+++ react_hooks_app/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+  <div className='jumbotron'>
+    <div className='container'>
+      <h1 className='display-4'>React приложение</h1>
+      <p className='lead'>Версия 1.2.32</p>
+    </div>
+  </div>
+)
